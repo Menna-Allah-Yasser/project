18ef7f8cd74e41c5f78bda6e394cb298e0471f0f
Menna Allah Yasser <mennaallahyasser003@gmail.com>
2020-12-11 20:15:17 +0200

    add new file

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+ 	int num , sum=0;
+ 	int c=1;
+ 	cout<<"enter the num\n";
+ 	cin>>num;
+ 	while(c<=10)
+ 	sum+=num;
+ 	c++;
+ 	cout<<sum;
+ 	
+	return 0;
+}
